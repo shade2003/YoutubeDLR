3db3c1d5d4be4212a6b634c07033bca1d9128e9e
Shade2003 <45914737+shade2003@users.noreply.github.com>
2021-04-12 12:00:33 +0530

    Re-wrote the whole thing
    Added support for downloading playlists.

--- YoutubeDLR/main.py
+++ YoutubeDLR/main.py
@@ -1,22 +1,89 @@
-#main.py
+# Importing libraries
+import bs4 as bs
+import sys
+import urllib.request
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QUrl
+import pytube # library for downloading youtube videos
 
-from pytube import YouTube
 
-link = input("   Enter video's link ...\n   ")
-try:
-    video = YouTube(str(link))
-except:
-    print('     Something went wrong')
-    print('     Check if your link is valid')
-    exit('     Exiting app ...')
+class Page(QWebEnginePage):
+	def __init__(self, url):
+		self.app = QApplication(sys.argv)
+		QWebEnginePage.__init__(self)
+		self.html = ''
+		self.loadFinished.connect(self._on_load_finished)
+		self.load(QUrl(url))
+		self.app.exec_()
 
-res = input('   Choose the number of the resolution you like!\n   0: 360p\n   1: 720p\n   2: 1080p\n   ')
-options = [ '360p', '720p', '1080p' ]
+	def _on_load_finished(self):
+		self.html = self.toHtml(self.Callable)
+		print('Load finished')
 
-try:
-    chosen_stream = video.streams.filter(file_extension = 'mp4', resolution = options[int(res)])
-except:
-    print('This resolution is not available')
-    exit('Exiting app ...')
+	def Callable(self, html_str):
+		self.html = html_str
+		self.app.quit()
 
-download_stream = chosen_stream[0].download(r'C:\Users\shed2003\Videos\dw') #put in the destination/location of the final file.
+
+links = []
+
+
+def exact_link(link):
+	vid_id = link.split('=')
+	# print(vid_id)
+	str = ""
+	for i in vid_id[0:2]:
+		str += i + "="
+
+	str_new = str[0:len(str) - 1]
+	index = str_new.find("&")
+
+	new_link = "https://www.youtube.com" + str_new[0:index]
+	return new_link
+
+
+url = "https://www.youtube.com/watch?v=lcJzw0JGfeE&list=PLqM7alHXFySENpNgw27MzGxLzNJuC_Kdj"
+# Scraping and extracting the video
+# links from the given playlist url
+page = Page(url)
+count = 0
+
+soup = bs.BeautifulSoup(page.html, 'html.parser')
+for link in soup.find_all('a', id='thumbnail'):
+	
+	# not using first link because it is
+	# playlist link not particular video link
+	if count == 0:
+		count += 1
+		continue
+	else:
+		vid_src = link['href']
+		# print(vid_src)
+		# keeping the format of link to be
+		# given to pytube otherwise in some cases
+		new_link = exact_link(vid_src)
+		
+		# error might occur due to this
+		# print(new_link)
+		
+		# appending the link to the links array
+		links.append(new_link)
+
+# print(links)
+
+# downloading each video from
+# the link in the links array
+for link in links:
+	yt = pytube.YouTube(link)
+	
+	# Downloaded video will be the best quality video
+	stream = yt.streams.filter(progressive=True,
+				file_extension='mp4').order_by(
+				'resolution').desc().first()
+	try:
+		stream.download()
+		# printing the links downloaded
+		print("Downloaded: ", link)
+	except:
+		print('Some error in downloading: ', link)
